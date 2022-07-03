6d8ccaa45a3da967b33aa01b7d238185b8034d7a
Viktor <viktor.m.rumenov@gmail.com>
2022-07-03 17:59:17 +0200

    Wire everything in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Characters from "./Characters";
+import FetchCharacters from "./FetchCharacters";
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div className="Application">
+      <h1>Star Wars Characters</h1>
+      <FetchCharacters />
+      <Characters />
+    </div>
+  );
 };
 
 export default App;
